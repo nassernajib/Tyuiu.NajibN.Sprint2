856755f4492c529bb9e3b72eea8e5e5bf37fe9c5
нассер <@NASSER>
2025-04-05 15:41:51 +0500

    Task3

---
+++ Tyuiu.NajibNSprint2.Task3.V25.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.NajibNSprint2.Task3.V25.Lib
+{
+    public class DataService : ISprint2Task3V25
+
+        {
+            public double Calculate(double x)
+            {
+                {
+                    double y = 0;
+                    if (x > 1)
+                    {
+                        y = Math.Sin(Math.Pow(x, 3)) + Math.Pow((x + 1) / (x - 1), x);
+                    }
+                    else
+                    {
+                        if (x == 0)
+                        {
+                            y = ((x * x) - (Math.Cos(x * x * x))) / (x - 3);
+
+                        }
+                        else
+                        {
+                            if ((-29 < x) && (x < 2))
+                            {
+                                y = Math.Pow((x + (1 / x * x)), x);
+                            }
+                            else
+                            {
+                                if (x < -29)
+                                {
+                                    y = x + 15 + ((12 / x) * x);
+                                }
+                            }
+                        }
+                    }
+
+                    return Math.Round(y, 3);
+                }
+            }
+        } } 
---
+++ Tyuiu.NajibNSprint2.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NajibNSprint2.Task3.V25.Lib;
+namespace Tyuiu.NajibNSprint2.Task3.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double res = ds.Calculate(x);
+            double wait = 8.956;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.333;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -36;
+            double res = ds.Calculate(x);
+            double wait = -9;
+            Assert.AreEqual(wait, res);
+            {
+            }
+        }
+    }
+}
